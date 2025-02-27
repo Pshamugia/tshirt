ead3df007df51ab277b13ad9b2c6fd21b11c01ca
Paata Shamugia <pshamugia@gmail.com>
2025-02-27 06:57:35 +0400

    Updated UI and storage assets

---
+++ tshirt_new-main/resources/js/bootstrap.js
@@ -0,0 +1,5 @@
+import "bootstrap";
+import axios from "axios";
+window.axios = axios;
+window.axios.defaults.baseURL = window.location.origin;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
